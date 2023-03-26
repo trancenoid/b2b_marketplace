157e39a5b465bb79569b6a4f7953646ffe39b75a
trancenoid <shivansh.bhr@gmail.com>
2023-03-26 19:12:25 +0530

    fix AddProduct.js

--- frontend/src/components/Products/AddProduct.js
+++ frontend/src/components/Products/AddProduct.js
@@ -1,109 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, TextInput, Button, StyleSheet } from 'react-native';
-import { useNavigation, useParams } from '@react-navigation/native';
-import { getProduct, updateProduct } from '../../services/products';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { createProduct } from "../../services/products";
 
-const EditProduct = () => {
-  const navigation = useNavigation();
-  const { id } = useParams();
-  const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    price: '',
-  });
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const product = await getProduct(id);
-      setFormData({
-        name: product.name,
-        description: product.description,
-        price: product.price,
-      });
-    };
-    fetchProduct();
-  }, [id]);
-
-  const handleChange = (name, value) => {
-    setFormData({ ...formData, [name]: value });
-  };
+const AddProduct = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
-    await updateProduct(id, formData);
-    navigation.navigate('Home');
+    setLoading(true);
+    const data = new FormData();
+    data.append("name", name);
+    data.append("description", description);
+    data.append("price", price);
+    data.append("image", image);
+    try {
+      await createProduct(data);
+      // history.push("/");
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Edit Product</Text>
-      <View style={styles.form}>
-        <View style={styles.inputContainer}>
-          <Text style={styles.label}>Name:</Text>
-          <TextInput
-            style={styles.input}
-            name="name"
-            value={formData.name}
-            onChangeText={(value) => handleChange('name', value)}
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.label}>Description:</Text>
-          <TextInput
-            style={styles.input}
-            name="description"
-            value={formData.description}
-            onChangeText={(value) => handleChange('description', value)}
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.label}>Price:</Text>
-          <TextInput
-            style={styles.input}
-            name="price"
-            value={formData.price.toString()}
-            keyboardType="numeric"
-            onChangeText={(value) => handleChange('price', value)}
-          />
-        </View>
-        <Button title="Submit" onPress={handleSubmit} />
-      </View>
+    <View>
+      <Text>Add Product</Text>
+      <TextInput
+        placeholder="Name"
+        value={name}
+        onChangeText={(text) => setName(text)}
+        required
+      />
+      <TextInput
+        placeholder="Description"
+        value={description}
+        onChangeText={(text) => setDescription(text)}
+        required
+      />
+      <TextInput
+        placeholder="Price"
+        value={price}
+        onChangeText={(text) => setPrice(text)}
+        keyboardType="numeric"
+        required
+      />
+      <Button
+        title="Choose Image"
+        onPress={() => {
+          // TODO: Add code to pick image
+        }}
+      />
+      <Button
+        title={loading ? "Adding..." : "Add"}
+        onPress={handleSubmit}
+        disabled={loading}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  form: {
-    width: '80%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  label: {
-    marginRight: 10,
-    fontSize: 18,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#333',
-    padding: 5,
-    fontSize: 18,
-    borderRadius: 5,
-    flex: 1,
-  },
-});
-
-export default EditProduct;
+export default AddProduct;
